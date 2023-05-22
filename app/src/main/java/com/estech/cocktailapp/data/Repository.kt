ab7b08e008927479e34d08053762dde523d92880
Liberty Tamayo Martinez <liberty.tamayo@estech.local>
2023-05-22 14:34:34 +0200

    ApiServiceFirstCommit

---
+++ app/src/main/java/com/estech/cocktailapp/data/Repository.kt
@@ -0,0 +1,9 @@
+package com.estech.cocktailapp.data
+
+import com.estech.cocktailapp.network.RetrofitHelper
+
+class Repository {
+
+    private val retrofit = RetrofitHelper.getRetrofit()
+
+}
---
+++ app/src/main/java/com/estech/cocktailapp/network/ApiService.kt
@@ -0,0 +1,4 @@
+package com.estech.cocktailapp.network
+
+interface ApiService {
+}
