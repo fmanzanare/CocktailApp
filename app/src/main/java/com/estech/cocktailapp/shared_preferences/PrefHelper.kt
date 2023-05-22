a0771215b5a3ef3c607b389939a70286b60070e4
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2023-05-22 14:35:20 +0200

    login on develop

--- app/src/main/java/com/estech/cocktailapp/shared_preferences/PrefHelper.kt
+++ app/src/main/java/com/estech/cocktailapp/shared_preferences/PrefHelper.kt
@@ -1,9 +1,28 @@
 package com.estech.cocktailapp.shared_preferences
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 
 class PrefHelper(context: Context) {
 
+    val sharedPrefHelper = context.getSharedPreferences(
+        Constants.PREFER,
+        AppCompatActivity.MODE_PRIVATE
+    )
+    val editor = sharedPrefHelper.edit()
 
+    fun getUsername() = sharedPrefHelper.getString(Constants.USER, "")
+
+    fun putUsername(username: String) {
+        editor.putString(Constants.USER, username)
+        editor.apply()
+    }
+
+    fun getCurrDate() = sharedPrefHelper.getString(Constants.DATE, "")
+
+    fun putCurrDate(currDate: String) {
+        editor.putString(Constants.DATE, currDate)
+        editor.apply()
+    }
 
 }
