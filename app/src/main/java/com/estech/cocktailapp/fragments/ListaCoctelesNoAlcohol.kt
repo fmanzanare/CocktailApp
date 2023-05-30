6e1d28b5d99c4f14823f3ea653b5df884ad5abca
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2023-05-30 17:50:23 +0200

    ListaCoctelesNoAlohol commented to avoid errors

--- app/src/main/java/com/estech/cocktailapp/fragments/ListaCoctelesNoAlcohol.kt
+++ app/src/main/java/com/estech/cocktailapp/fragments/ListaCoctelesNoAlcohol.kt
@@ -5,37 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.estech.cocktailapp.R
-import com.estech.cocktailapp.databinding.FragmentSecondBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class ListaCoctelesNoAlcohol : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+//    private lateinit var _binding: FragmentSecondBinding
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//
+//        binding = FragmentSecondBinding.inflate(inflater, container, false)
+//        return (binding.root)
+//
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//    }
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
