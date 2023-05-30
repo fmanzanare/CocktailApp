b5f7391e91776e217ec492ddd656b80461457631
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2023-05-30 19:12:18 +0200

    listado cocteles no Alcoholic on develop

--- app/src/main/java/com/estech/cocktailapp/fragments/ListaCoctelesNoAlcohol.kt
+++ app/src/main/java/com/estech/cocktailapp/fragments/ListaCoctelesNoAlcohol.kt
@@ -5,23 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.estech.cocktailapp.databinding.ListaCoctelesBinding
 
 class ListaCoctelesNoAlcohol : Fragment() {
 
-//    private lateinit var _binding: FragmentSecondBinding
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//
-//        binding = FragmentSecondBinding.inflate(inflater, container, false)
-//        return (binding.root)
-//
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//    }
+    private lateinit var binding: ListaCoctelesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = ListaCoctelesBinding.inflate(inflater, container, false)
+        return (binding.root)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
