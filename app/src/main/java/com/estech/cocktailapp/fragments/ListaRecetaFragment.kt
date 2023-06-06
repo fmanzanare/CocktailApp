6f806934fdf7dd7ea7c73137e24b3cea71343643
Liberty <liberty.tamayo@escuelaestech.es>
2023-06-06 19:03:21 +0200

    view pager on detalle coctel implemmented

---
+++ app/src/main/java/com/estech/cocktailapp/fragments/ListaRecetaFragment.kt
@@ -0,0 +1,26 @@
+package com.estech.cocktailapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.estech.cocktailapp.databinding.ListaRecetaBinding
+
+class ListaRecetaFragment : Fragment() {
+
+    private lateinit var binding: ListaRecetaBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ListaRecetaBinding.inflate(inflater, container, false)
+        return (binding.root)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
